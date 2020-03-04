fa6e530ac4067c8a6666ae32d7824db22e43f932
Remy Bourdoncle <S226294@siege.axa-fr.intraxa>
2020-03-04 09:37:07 +0100

    feat(client): liste des ouvrages

--- eLitClient/src/main/java/fr/rbo/elitweb/controller/OuvragesController.java
+++ eLitClient/src/main/java/fr/rbo/elitweb/controller/OuvragesController.java
@@ -5,10 +5,12 @@ import fr.rbo.elitweb.proxies.APIProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -18,16 +20,29 @@ public class OuvragesController {
     APIProxy apiProxy;
 
     @RequestMapping(value="/ouvrages", method = RequestMethod.GET)
-    public String accueil(Model model){
+    public String Ouvrages(Model model, HttpSession httpSession){
+        OuvrageBean ouvrageCriteres = new OuvrageBean();
         List<OuvrageBean> ouvrages = apiProxy.findAll();
+        model.addAttribute("status", "/ouvrages");
+        model.addAttribute("ouvrageCriteres", ouvrageCriteres);
         model.addAttribute("ouvrages", ouvrages);
-        return "ouvrages";
+        return "recherche-ouvrages-list";
     }
-    @RequestMapping(value = "/details", method = RequestMethod.GET)
+    @RequestMapping(value="/ouvrages/recherche", method = RequestMethod.POST)
+    public String OuvragesRecherche (Model model,
+                                     @ModelAttribute("ouvrageCriteres") OuvrageBean ouvrageCriteres,
+                                     HttpSession httpSession) {
+        List<OuvrageBean> ouvrages = apiProxy.findAll();
+        model.addAttribute("status", "/ouvrages/recherche");
+        model.addAttribute("ouvrageCriteres", ouvrageCriteres);
+        model.addAttribute("ouvrages", ouvrages);
+        return "recherche-ouvrages-list";
+    }
+    @RequestMapping(value = {"/details", "/ouvrage/details"}, method = RequestMethod.GET)
     public String details(@RequestParam("ouvrageId") int ouvrageId, Model model){
         OuvrageBean ouvrage = apiProxy.findById(ouvrageId);
         model.addAttribute("ouvrage", ouvrage);
-        return "details";
+        return "ouvrage-details";
     }
 
 
